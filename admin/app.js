3b0e66bca5178733c3433d271b7c6934c0fba5ef
Weckmann <clark@weckmann.me>
2021-11-20 14:13:14 -0600

    automated push for new post

--- admin/app.js
+++ admin/app.js
@@ -1,4 +1,13 @@
 var execProcess = require("./exec_process.js");
+const fs = require('fs')
+const content = 'Some content!'
+fs.writeFile('../posts/demo.md', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+})
 execProcess.result("sh gitpush.sh", function(err, response){
     if(!err){
         console.log(response);
